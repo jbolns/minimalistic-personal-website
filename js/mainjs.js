b7f5dd0cd41b8d97f3ed2dc4f7c4974b42acde9b
Jose A Bolanos <80164315+jbolns@users.noreply.github.com>
2021-08-31 15:53:56 +0300

    Add files via upload

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,7 +1,7 @@
 // Main JS File
 function loadReload(){
   var str = $(location).attr('hash');
-  str = str.substring(1)
+  str = str.substring(1);
   if ( str === "") {
     $('#wrapper').load('includes/home.html');
   } else {
@@ -34,35 +34,7 @@ function closeMenu(){
   $('.socialMenu').css('display','inline-block');
 }
 
-function giveMeMore(elem){
-  $(elem).toggleClass("active");
-}
-
 function iLikeYou(){
-  alert ("You rebel! I like you!")
-  $('.redButton').css('display', 'none')
-}
-
-function centrao(){
-  var bodyHeight = $(window).height() - $('nav').height() - $('footer').height();
-  var wrapperHeight = $('header').height() + $('main').height();
-  var centrePage = (bodyHeight - wrapperHeight)/2;
-  var asideHeight = $('aside').height();
-  var windowWidth = $(window).width();
-  $('#wrapper').css('marginTop', '0');
-  $('#avatar').css('marginTop', '14px');
-  if (windowWidth > 768) {
-    if (wrapperHeight < bodyHeight) {
-      $('#wrapper').css('marginTop', centrePage);
-    };
-    if (centrePage > asideHeight/2) {
-      $('#avatar').css('marginTop', centrePage - asideHeight/3);
-    };
-  }
-}
-
-function blogCentrao(){
-  var asideHeight = $('aside').height();
-  $('#avatar').css('marginTop', '14px');
-  $('#wrapper').css('marginTop', '14px');
+  alert ("You rebel! I like you!");
+  $('.greenButton').css('display', 'none');
 }
